50076ed7b42bc30beeab9e99043f08ecf7392e95
TJ Saunders <tj@castaglia.org>
2017-03-29 09:32:53 -0700

    Updating mod_info for the current codebase.

--- mod_info.h
+++ mod_info.h
@@ -1,7 +1,7 @@
 /*
  * ProFTPD: mod_info -- a module implementing informational SITE commands
  *
- * Copyright (c) 2003-2009 TJ Saunders
+ * Copyright (c) 2003-2017 TJ Saunders
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,8 +24,6 @@
  *
  * This is mod_info, contrib software for proftpd 1.2 and above.
  * For more information contact TJ Saunders <tj@castaglia.org>.
- *
- * $Id: mod_info.h,v 1.5 2004/07/14 17:12:24 tj Exp tj $
  */
 
 #ifndef MOD_INFO_H
@@ -34,11 +32,11 @@
 #include "conf.h"
 #include "privs.h"
 
-#define MOD_INFO_VERSION	"mod_info/0.4"
+#define MOD_INFO_VERSION	"mod_info/0.5"
 
 /* Make sure the version of proftpd is as necessary. */
-#if PROFTPD_VERSION_NUMBER < 0x0001030301
-# error "ProFTPD 1.3.3rc1 or later required"
+#if PROFTPD_VERSION_NUMBER < 0x0001030501
+# error "ProFTPD 1.3.5rc1 or later required"
 #endif
 
 /* Statistics objects */
